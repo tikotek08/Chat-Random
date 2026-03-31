46e6eeeb84a5fe91d89bf17e1624564792f24fa6
Heriberto <tiko.tek08@gmail.com>
2026-03-30 20:28:35 -0400

    chore: simplify auth config

--- auth.ts
+++ auth.ts
@@ -3,12 +3,4 @@ import Google from 'next-auth/providers/google'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  callbacks: {
-    session({ session, token }) {
-      if (session.user && token.sub) {
-        session.user.id = token.sub
-      }
-      return session
-    },
-  },
 })
